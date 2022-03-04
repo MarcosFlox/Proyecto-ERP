ed9cdece1fa39fb68813c3199b0629252eadbcf5
MarcosFlox <81320522+MarcosFlox@users.noreply.github.com>
2022-03-04 20:01:05 +0100

    Eliminada clase StoreHouseException
    He eliminado la clase StoreHouseException, ahora las excepciones heredan directamente de BaseException ya que con la otra daba errores, no mostraba bien lo que quería.
    Añadido los exports.

--- exceptions.js
+++ exceptions.js
@@ -13,7 +13,7 @@ class BaseException extends Error {
 //Excepción acceso inválido a constructor
 class InvalidAccessConstructorException extends BaseException {
 	constructor(fileName, lineNumber) {
-		super("Constructor can’t be called as a function.", fileName, lineNumber);
+		super("Constructor can't be called as a function.", fileName, lineNumber);
 		this.name = "InvalidAccessConstructorException";
 	}
 }
@@ -39,76 +39,100 @@ class InvalidValueException extends BaseException {
 //Excepción personalizada para clases abstractas.
 class AbstractClassException extends BaseException {
 	constructor(className, fileName, lineNumber) {
-		super(`Error: The class  ${className} is abstract.`, fileName, lineNumber);
+		super(`Error: The class ${className} is abstract.`, fileName, lineNumber);
 		this.className = className;
 		this.name = "AbstractClassException";
 	}
 }
 
-class StoreHouseException extends BaseException {
-	constructor(fileName, lineNumber) {
-		super("Error: StoreHouse Exception.", fileName, lineNumber);
-		this.name = "StoreHouseException";
-	}
-}
+// class StoreHouseException extends BaseException {
+// 	constructor(fileName, lineNumber) {
+// 		super("Error: StoreHouse Exception.", fileName, lineNumber);
+// 		this.name = "StoreHouseException";
+// 	}
+// }
 
-class CategoryStoreHouseException extends StoreHouseException {
+class CategoryStoreHouseException extends BaseException {
 	constructor(fileName, lineNumber) {
 		super("Error: The method needs a Category parameter.", fileName, lineNumber);
 		this.name = "CategoryStoreHouseException";
 	}
 }
 
-class CategoryExistsException extends StoreHouseException {
+class CategoryExistException extends BaseException {
 	constructor(category, fileName, lineNumber) {
-		super("Error: The category exists in the storehouse yet. " + category.title, fileName, lineNumber);
-		this.name = "ProductStoreHouseException";
+		super("Error: The category exists in the storehouse yet: " + category.title, fileName, lineNumber);
+		this.name = "CategoryExistException";
 		this.category = category;
 	}
 }
 
-class CategoryNotExistException extends StoreHouseException {
+class CategoryNotExistException extends BaseException {
 	constructor(category, fileName, lineNumber) {
-		super("Error: The category doesn't exist in storehouse. " + category.title, fileName, lineNumber);
+		super("Error: The category doesn't exist in storehouse: " + category.title, fileName, lineNumber);
 		this.name = "CategoryShoppingCartException";
 		this.category = category;
 	}
 }
 
-class ProductStoreHouseException extends StoreHouseException {
-	constructor (fileName, lineNumber){
+class ProductStoreHouseException extends BaseException {
+	constructor(fileName, lineNumber) {
 		super("Error: The method needs a Product parameter.", fileName, lineNumber);
 		this.name = "ProductStoreHouseException";
-	} 
+	}
 }
 
-class ProductNotExistException extends StoreHouseException {
-	constructor (product, fileName, lineNumber){
-		super("Error: The product doesn't exist in the storehouse. " + product.serial, fileName, lineNumber);
+class ProductExistException extends BaseException {
+	constructor(product, fileName, lineNumber) {
+		super("Error: The product exists in the storehouse yet: " + product.name, fileName, lineNumber);
 		this.name = "ProductStoreHouseException";
 		this.product = product;
 	}
 }
 
-class StoreStoreHouseException extends StoreHouseException {
-	constructor (fileName, lineNumber){
+class ProductNotExistException extends BaseException {
+	constructor(product, fileName, lineNumber) {
+		super("Error: The product doesn't exist in the storehouse: " + product.name, fileName, lineNumber);
+		this.name = "ProductStoreHouseException";
+		this.product = product;
+	}
+}
+
+class StoreStoreHouseException extends BaseException {
+	constructor(fileName, lineNumber) {
 		super("Error: The method needs a Store parameter.", fileName, lineNumber);
 		this.name = "StoreStoreHouseException";
-	} 
+	}
 }
 
-class StoreNotExistException extends StoreHouseException {
-	constructor (store, fileName, lineNumber){
-		super("Error: The store doesn't exist in the storehouse. " + store.cif, fileName, lineNumber);
+class StoreNotExistException extends BaseException {
+	constructor(store, fileName, lineNumber) {
+		super("Error: The store doesn't exist in the storehouse: " + store.name, fileName, lineNumber);
 		this.name = "StoreStoreHouseException";
 		this.store = store;
 	}
 }
 
-class StoreExistException extends StoreHouseException {
-	constructor (store, fileName, lineNumber){
-		super("Error: The store exists in the storehouse yet. " + store.cif, fileName, lineNumber);
+class StoreExistException extends BaseException {
+	constructor(store, fileName, lineNumber) {
+		super("Error: The store exists in the storehouse yet: " + store.name, fileName, lineNumber);
 		this.name = "StoreStoreHouseException";
 		this.store = store;
 	}
 }
+export{
+	BaseException,
+	InvalidAccessConstructorException,
+	EmptyValueException,
+	InvalidValueException,
+	AbstractClassException,
+	CategoryStoreHouseException,
+	CategoryExistException,
+	CategoryNotExistException,
+	ProductStoreHouseException,
+	ProductExistException,
+	ProductNotExistException,
+	StoreStoreHouseException,
+	StoreNotExistException,
+	StoreExistException
+};
